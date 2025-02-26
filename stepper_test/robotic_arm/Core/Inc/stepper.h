a76c40d02254a79cc50c3b821ff9a75e84462c97
Davide Tonti <davidet952@gmail.com>
2025-02-26 01:53:54 +0100

    stepper_test_1

---
+++ stepper_test/robotic_arm/Core/Inc/stepper.h
@@ -0,0 +1,42 @@
+/*
+ * stepper.h
+ *
+ *  Created on: Feb 26, 2025
+ *      Author: david
+ */
+
+#ifndef INC_STEPPER_H_
+#define INC_STEPPER_H_
+#include "main.h"
+
+typedef struct {
+	GPIO_TypeDef *timer_pwm_ch; //timer_pwm_channel
+	float freq_steps;
+	//
+	GPIO_TypeDef *enable_port;
+	GPIO_TypeDef *direction_port;
+	//
+	uint16_t stepper_resolution;
+	uint16_t step_per_rev; // = 360/stepper_resolution; // 360°/resolution
+	uint16_t microstep;
+	uint16_t step_scale; //= step_per_rev*microstep;
+
+} stepper_obj;
+
+//calcoli gli step necessari per il dato angolo da:
+//N_STEPS=STEP_SCALE*DISTANCE_INPUT
+
+void stepper_init(stepper_obj *stp, GPIO_TypeDef *timer_pwm_ch,
+		float stepper_resolution, uint16_t microstep, GPIO_TypeDef *enable_port,
+		GPIO_TypeDef *direction_port) ;
+
+void stepper_move(stepper_obj *stp, float pwm_port, float dir, uint8_t enable,
+		float position, float freq_steps);
+
+/* Useful getter functions to access the parameters of the PWM signal
+ uint32_t nidec_h24_GetPeriod();
+ uint32_t nidec_h24_GetPrescaler();
+ uint32_t nidec_h24_GetCounter();
+ uint32_t nidec_h24_GetDutyCycle();*/
+
+#endif /* INC_STEPPER_H_ */
---
+++ stepper_test/robotic_arm/Core/Src/stepper.c
@@ -0,0 +1,30 @@
+#include <stepper.h>
+
+extern TIM_HandleTypeDef htim3;
+
+//definisco l'oggetto stepper con i seguenti parametri
+
+void stepper_init(stepper_obj *stp, GPIO_TypeDef *timer_pwm_ch,
+		float stepper_resolution, uint16_t microstep, GPIO_TypeDef *enable_port,
+		GPIO_TypeDef *direction_port) {
+
+	stp->timer_pwm_ch = timer_pwm_ch;
+	stp->enable_port = enable_port;
+	stp->direction_port = direction_port;
+
+	stp->stepper_resolution = stepper_resolution;
+	stp->microstep = microstep;
+	stp->step_per_rev = 360.0f / stepper_resolution; // 360°/resolution
+	stp->step_scale = stp->step_per_rev * microstep;
+}
+
+void stepper_move(stepper_obj *stp, float pwm_port, float direction,
+		uint8_t enable, float position, float freq_steps) {
+
+	float n_steps;
+	//calcoli gli step necessari per il dato angolo da:
+	n_steps = stp->step_scale * position;
+
+	//N_STEPS=STEP_SCALE*DISTANCE_INPUT
+}
+
