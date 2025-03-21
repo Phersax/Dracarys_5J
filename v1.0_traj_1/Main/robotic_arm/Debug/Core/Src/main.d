abdbc651ee6790a1ef1b431a312c3bad056bdd09
Davide Tonti <davidet952@gmail.com>
2025-03-21 15:51:21 +0100

    updated v1.0_traj_1
    --deleted gpio_end_eff lib for that type of endeff

--- v1.0_traj_1/Main/robotic_arm/Debug/Core/Src/main.d
+++ v1.0_traj_1/Main/robotic_arm/Debug/Core/Src/main.d
@@ -27,7 +27,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
  ../Core/Inc/tim.h ../Core/Inc/main.h ../Core/Inc/gpio.h \
- ../Core/Inc/stepper.h ../Core/Inc/servo.h ../Core/Inc/end_eff_gpio.h \
+ ../Core/Inc/stepper.h ../Core/Inc/servo.h \
  ../Core/Inc/trapezoidal_profile.h ../Core/Src/trapezoidal_profile.c
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
@@ -62,6 +62,5 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Core/Inc/gpio.h:
 ../Core/Inc/stepper.h:
 ../Core/Inc/servo.h:
-../Core/Inc/end_eff_gpio.h:
 ../Core/Inc/trapezoidal_profile.h:
 ../Core/Src/trapezoidal_profile.c:
--- v1.0_traj_1/Main/robotic_arm/Debug/Core/Src/subdir.mk
+++ v1.0_traj_1/Main/robotic_arm/Debug/Core/Src/subdir.mk
@@ -5,7 +5,6 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Core/Src/end_eff_gpio.c \
 ../Core/Src/gpio.c \
 ../Core/Src/main.c \
 ../Core/Src/servo.c \
@@ -18,7 +17,6 @@ C_SRCS += \
 ../Core/Src/tim.c 
 
 OBJS += \
-./Core/Src/end_eff_gpio.o \
 ./Core/Src/gpio.o \
 ./Core/Src/main.o \
 ./Core/Src/servo.o \
@@ -31,7 +29,6 @@ OBJS += \
 ./Core/Src/tim.o 
 
 C_DEPS += \
-./Core/Src/end_eff_gpio.d \
 ./Core/Src/gpio.d \
 ./Core/Src/main.d \
 ./Core/Src/servo.d \
@@ -51,7 +48,7 @@ Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/end_eff_gpio.cyclo ./Core/Src/end_eff_gpio.d ./Core/Src/end_eff_gpio.o ./Core/Src/end_eff_gpio.su ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/servo.cyclo ./Core/Src/servo.d ./Core/Src/servo.o ./Core/Src/servo.su ./Core/Src/stepper.cyclo ./Core/Src/stepper.d ./Core/Src/stepper.o ./Core/Src/stepper.su ./Core/Src/stm32f4xx_hal_msp.cyclo ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_it.cyclo ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su ./Core/Src/tim.cyclo ./Core/Src/tim.d ./Core/Src/tim.o ./Core/Src/tim.su
+	-$(RM) ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/servo.cyclo ./Core/Src/servo.d ./Core/Src/servo.o ./Core/Src/servo.su ./Core/Src/stepper.cyclo ./Core/Src/stepper.d ./Core/Src/stepper.o ./Core/Src/stepper.su ./Core/Src/stm32f4xx_hal_msp.cyclo ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_it.cyclo ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su ./Core/Src/tim.cyclo ./Core/Src/tim.d ./Core/Src/tim.o ./Core/Src/tim.su
 
 .PHONY: clean-Core-2f-Src
 
